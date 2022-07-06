d1fceeec71bae45b949d9b0f4e8f583648223dfc
Krishna-chitragar <chitragarkrishna@gmail.com>
2022-07-06 12:00:00 +0530

    adding source code for the Project

---
+++ NFT-marketplace/nft-marketplace-starter-kit-main/src/contracts/KryptoBirdz.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Kryptobird {
+
+    string public name;
+    string public symbol;
+    string public sym;
+
+    constructor(){
+        name = 'Kryptobird';
+        symbol = 'KBIRDZ';
+        sym = "symi";
+        
+    }
+
+}
+
+
+
